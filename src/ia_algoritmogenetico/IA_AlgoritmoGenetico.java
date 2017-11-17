f870542f1d10f86cb1e196206146b59a19846c12
josuesan <josueguitar2@gmail.com>
2017-11-16 23:52:48 -0400

    re

--- src/ia_algoritmogenetico/IA_AlgoritmoGenetico.java
+++ src/ia_algoritmogenetico/IA_AlgoritmoGenetico.java
@@ -11,17 +11,75 @@ public class IA_AlgoritmoGenetico {
     public static int W; //Tamaño del jardín
     public static int H; //Cantidad máxima de plantas
     public static int Z; //Cantidad de zombies
-    public static char Jardin [][];  //Matriz que representa el Jardin
+    public static Individuo Jardin [][];  //Matriz que representa el Jardin
     public static List<Zombie> Zombies = new ArrayList<Zombie>();  
 
+    public static double RamdonNumber(int max, int min){
+        int range = (max - min) ;     
+        return (Math.random() * range) + min;
+    }
+    public static int binaryToDecimal(int number []) {
+        int decimal = 0;
+        int tam = number.length;
+        int power = tam-1;
+        for (int i =0; i<tam ; i++){
+            decimal += number[i]* Math.pow(2, power);
+            power--;
+        }
+        return decimal;
+    }
+    public static void algoritmoGenetico (){
+        inicializarPoblacion();
+        evaluarPoblacion();
+        while(! solucion()){
+            seleccionar();
+            alterar();
+            evaluarPoblacion();
+        }
+        return;
+    }
+    public static void inicializarPoblacion (){
+        int i,j;
+        for (i=0; i< H; i++){
+            for(j=0; j<4; j++){
+                Jardin[i][j] = GeneradorIndividuo();
+            }
+        }
+    }
+    public static Individuo GeneradorIndividuo(){
+        Individuo planta = new Individuo();
+        return planta;
+    }
+    public static void evaluarPoblacion (){
+        
+    }
+    public static boolean solucion (){
+        
+    }
+    public static void seleccionar (){
+        
+    }
+    public static void alterar (){
+        
+    }
+    public static void simulacion(){
+        
+        while(hayZombies() && !zombiesWin() ){
+            //Plantas disparan
+            //aumentar contador de daño causado de planta
+            //Nuevo zombie entra
+            //Zombies caminan o Zombies atacan
+            
+        }
+    }
     public static void main(String[] args) {
         //Leyendo la entrada
-        Scanner consola = new Scanner(System.in);
+       /* Scanner consola = new Scanner(System.in);
         W = consola.nextInt(); //Tamaño del jardin
         H = consola.nextInt(); //Cantidad máxima de plantas
         Z = consola.nextInt(); //Cantidad de zombies
         
-        Jardin = new char[4][H]; //Le damos dimensiones al jardin
+        Jardin = new Individuo[4][H]; //Le damos dimensiones al jardin
         
         
         int ZF, ZV;
@@ -33,7 +91,7 @@ public class IA_AlgoritmoGenetico {
             zombito = new Zombie(ZF,ZV,i); 
             
             Zombies.add(zombito); //Añadimos el zombie número i 
-        }   
+        }   */  
     }    
 }
 
@@ -52,4 +110,16 @@ class Zombie{
         this.vida = vida;
         this.orden = orden;
     }
+}
+
+class Individuo{
+    public int cromosoma [] = new int [2]; 
+    public int id;
+    Individuo(){
+        for (int i =0; i<2; i++){
+            this.cromosoma[i] = (int) Math.round( IA_AlgoritmoGenetico.RamdonNumber(0,1) ) ; 
+        }
+        this.id = IA_AlgoritmoGenetico.binaryToDecimal(this.cromosoma);
+    }
+    
 }
